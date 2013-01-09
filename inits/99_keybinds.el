92c9a0d3fa4581ddb3916f2fcae4cca947675938
Alexander Goldberg <algold@gmail.com>
2013-01-09 15:35:15 +0200

    Remove some unworking deinitions

--- inits/99_keybinds.el
+++ inits/99_keybinds.el
@@ -29,38 +29,3 @@
 ;Use <Leader>' to bring up emacs command mode with helm
 ;Should replace with single stroke, when I decide on one
 (evil-leader/set-key "'" 'helm-M-x)
-
-;Can be used in all modes
-(defun move-line-down ()
-  (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines 1))
-    (forward-line)
-    (move-to-column col)))
-(defun move-line-up ()
-  (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines -1))
-    (move-to-column col)))
-(global-set-key (kbd "<C-S-down>") 'move-line-down)
-(global-set-key (kbd "<C-S-up>") 'move-line-up)
-
-(defun open-line-below ()
-  (interactive)
-  (end-of-line)
-  (newline)
-  (indent-for-tab-command))
-
-(defun open-line-above ()
-  (interactive)
-  (beginning-of-line)
-  (newline)
-  (forward-line -1)
-  (indent-for-tab-command))
-
-(global-set-key (kbd "<C-return>") 'open-line-below)
-(global-set-key (kbd "<C-S-return>") 'open-line-above)
