2263382696150f28ce9b39585be8c54eb3eda67a
Alexander Goldberg <algold@gmail.com>
2013-01-10 16:21:55 +0200

    disable empty lines

--- inits/10_global.el
+++ inits/10_global.el
@@ -1,2 +1,8 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
+
+;; Disable fringe indicator of empty lines
+(setq-default  indicate-empty-lines nil)
+
+; Disable end-of-buffer indicator
+(setq-default  indicate-buffer-boundaries nil)
