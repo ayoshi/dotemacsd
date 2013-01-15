06e7357d03b6b9d720d91a584b43526903293502
Alexander Goldberg <alexg@infinidat.com>
2013-01-15 13:41:44 +0200

    Another take on bakcup files

--- inits/00_init.el
+++ inits/00_init.el
@@ -45,18 +45,16 @@
 
 (progn
   (setq make-backup-files t)
-  ;; Don't clutter up directories with files~
-  (setq backup-directory-alist `(("." . ,(expand-file-name
-                                          (concat dotfiles-dir "backups")))))
 
-  ;; Don't clutter with #files either
-  (setq auto-save-file-name-transforms
-        `((".*" ,(expand-file-name (concat dotfiles-dir "backups")))))
+  (defvar backup-dir (expand-file-name "~/.emacs.d/backup/"))
+  (defvar autosave-dir (expand-file-name "~/.emacs.d/backup/"))
+  (setq backup-directory-alist (list (cons ".*" backup-dir)))
+  (setq auto-save-list-file-prefix autosave-dir)
+  (setq auto-save-file-name-transforms `((".*" ,autosave-dir t)))
   (setq version-control t)
   (setq kept-new-versions 2)
   (setq kept-old-versions 2)
-  (setq delete-old-versions t)
-  )
+  (setq delete-old-versions t))
 
 ;; Save point position between sessions
 (require 'saveplace)
