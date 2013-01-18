5e77f4246fc162a4073b0209a2557116bd95f097
Alexander Goldberg <algold@gmail.com>
2013-01-18 15:47:43 +0200

    * inits/10_autocomplete.el: Bind Alt-Tab to autocomlete

--- inits/10_autocomplete.el
+++ inits/10_autocomplete.el
@@ -3,3 +3,9 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (require 'auto-complete-config)
 (ac-config-default)
+
+;Use tab to autocomplete
+(ac-set-trigger-key "TAB")
+
+; Bind Alt-Tab to autocomplete
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
