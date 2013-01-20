249ddd8b27fb81e192f61f5bb6eadebb2dac429e
Alexander Goldberg <alexg@infinidat.com>
2013-01-20 18:08:22 +0200

    Fix ac-dict

--- inits/10_autocomplete.el
+++ inits/10_autocomplete.el
@@ -1,8 +1,9 @@
 ;; ;;; 10_autocomplete.el ---
 
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (require 'auto-complete-config)
 (ac-config-default)
+
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 
 ;Use tab to autocomplete
 (ac-set-trigger-key "TAB")
