980f0dee1b22b6edc9da8385763a2c83103f213a
Alexander Goldberg <algold@gmail.com>
2013-01-29 00:01:56 +0200

    * inits/10_evil.el: Make space/backspace move 10 lines. Disable Return insert

--- inits/10_evil.el
+++ inits/10_evil.el
@@ -32,3 +32,20 @@
                               (semantic-symref-results-mode . emacs)
                               (rdictcc-buffer-mode . emacs))
       do (evil-set-initial-state mode state))
+
+; simulate vim's "nnoremap <space> 10jzz"
+(define-key evil-normal-state-map " " (lambda ()
+                     (interactive)
+                     (next-line 10)
+                     (evil-scroll-line-down 10)
+                     ))
+
+; simulate vim's "nnoremap <backspace> 10kzz"
+(define-key evil-normal-state-map [backspace] (lambda ()
+                     (interactive)
+                     (previous-line 10)
+                     (evil-scroll-line-up 10)
+                     ))
+
+; Remove annoying Return inserts
+(define-key evil-normal-state-map [return] "")
